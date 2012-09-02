13520b4ee5e4b90c5d42bdca8120d2629691774a
lusaisai <lusaisai@163.com>
2012-09-02 10:34:51 +0800

    fix audacious start delay issue

--- bin/audioRandomPlay.sh
+++ bin/audioRandomPlay.sh
@@ -1,9 +1,8 @@
 #! /bin/bash -e
 
 retry_times=10
-song_num=${1:-15}
+song_num=${1:-15} # play 15 songs by default
 song_index=0
-song_string=""
 audio_dir=/media/Collection/Music
 cd $audio_dir
 
@@ -11,7 +10,7 @@ i=0
 while [ $song_index -lt $song_num ]
 do
    if [ $i -gt $retry_times ]; then
-      cd $audio_dir
+      cd $audio_dir # In case of going into a dir whthout musics
       i=0
    else
       ((i += 1))
@@ -21,10 +20,12 @@ do
    file="$( ls -1 | sed ''$random_num' !d' )"
    is_audio=$(echo "$file" | perl -e "while(<>) { if ( m/(mp3|ape|flac|m4a)$/i) {print "1";} else {print "0"} }")
    if [ $is_audio -eq 1 -a -f "$file" ]; then
-      echo "Add song $file ..."
       if [ $song_index -eq 0 ]; then
+         echo "Play song $file ..."
          audacious "$file" > /dev/null 2>&1 &
+         sleep 1 # Give audacious some time to start up
       else
+         echo "Add song $file ..."
          audacious -e "$file" &
       fi
       cd $audio_dir
@@ -35,5 +36,3 @@ do
       continue
    fi
 done
-
-
