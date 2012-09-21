450bceebc0acb3bf8db349d41753c590781fc5bd
lusaisai <lusaisai@163.com>
2012-09-21 20:13:36 +0800

    test

--- bin/test.py
+++ bin/test.py
@@ -1,11 +1,8 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 """
-Created on Fri Sep 21 19:37:28 2012
+Created on Fri Sep 21 20:13:20 2012
 
 @author: lusaisai
 """
 
-import os
-
-os.listdir('/home/lusaisai/Videos/Anri Sugihara 杉原杏璃')
