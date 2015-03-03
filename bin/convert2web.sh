efeb23c284f388587dd6497a40a361c0f15a6b21
lusaisai <lusaisai@163.com>
2015-03-03 17:54:34 +0800

    add 'reduce rate' option

--- bin/convert2web.sh
+++ bin/convert2web.sh
@@ -14,9 +14,29 @@ function info {
 	echo -e "\e[34m$@\e[39m"
 }
 
+# getopts
+REDUCE_FACTOR=1
+while getopts ":r:" opt; do
+  case $opt in
+    r)
+      REDUCE_FACTOR=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an reduce factor." >&2
+      exit 1
+      ;;
+  esac
+done
+
+shift $((OPTIND-1))
+
 # command line parameters
 if [[ $# < 1 ]]; then
-	error "`basename $0` <filename> [webm|mp4]"
+	error "`basename $0` [-r <2>] <filename> [webm|mp4]"
 	exit 1
 fi
 
@@ -31,15 +51,15 @@ fi
 # other parameters
 TARGET_CODEC=${2:-mp4}
 TARGET_DIR=/mnt/ent
-TARGET_FILE=$TARGET_DIR/"`basename $INPUT_FILE | sed 's/\.[^.]*$/\./' `$TARGET_CODEC"
+TARGET_FILE=$TARGET_DIR/"`basename "$INPUT_FILE" | sed 's/\.[^.]*$/\./' `$TARGET_CODEC"
 
 # detect input file info
 info "Detecting the file ..."
-TOTAL_RATE=`ffprobe $INPUT_FILE 2>&1 | sed '/Duration/ !d' \
+TOTAL_RATE=`ffprobe "$INPUT_FILE" 2>&1 | sed '/Duration/ !d' \
 	|  awk -F"," '{ for(i = 1; i <= NF; i++) { if( $i ~ /kb\/s/ ) {gsub(/[^0-9]+/, "", $i); print $i;}  } }'`
-VIDEO_RATE=`ffprobe $INPUT_FILE 2>&1 | sed '/Video/ !d' \
+VIDEO_RATE=`ffprobe "$INPUT_FILE" 2>&1 | sed '/Video/ !d' \
 	|  awk -F"," '{ for(i = 1; i <= NF; i++) { if( $i ~ /kb\/s/ ) {gsub(/[^0-9]+/, "", $i); print $i;}  } }'`
-AUDIO_RATE=`ffprobe $INPUT_FILE 2>&1 | sed '/Audio/ !d' \
+AUDIO_RATE=`ffprobe "$INPUT_FILE" 2>&1 | sed '/Audio/ !d' \
 	|  awk -F"," '{ for(i = 1; i <= NF; i++) { if( $i ~ /kb\/s/ ) {gsub(/[^0-9]+/, "", $i); print $i;}  } }'`
 
 if [[ -z $AUDIO_RATE  ]]; then
@@ -59,6 +79,9 @@ if [[ -z $VIDEO_RATE  ]]; then
 		read VIDEO_RATE
 	fi
 fi
+
+VIDEO_RATE=$((VIDEO_RATE / REDUCE_FACTOR))
+
 
 info "Converting, it will take some time, please wait ..."
 time ffmpeg -i "$INPUT_FILE" -b:v "${VIDEO_RATE}k" -b:a "${AUDIO_RATE}k" -threads `nproc` "$TARGET_FILE"
